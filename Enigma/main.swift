d61a39431adbaf49e84bfba6c3c1d065df8a1fca
LEOYoon-Tsaw <lyc20041@gmail.com>
2015-04-18 23:20:11 +0800

    Initial Commit

---
+++ Enigma/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Enigma
+//
+//  Created by LEO Yoon-Tsaw on 18/4/15.
+//  Copyright (c) 2015年 Yoon-Tsaw Leo. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
